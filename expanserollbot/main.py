8bd1688e1fe04761159c5f3bdebbc0c021483697
Mike Lang <mikelang3000@gmail.com>
2019-03-06 03:23:41 -0800

    Implement !test

--- expanserollbot/main.py
+++ expanserollbot/main.py
@@ -1,5 +1,7 @@
 
 import logging
+import random
+from collections import Counter
 
 import discord
 import pyhedrals
@@ -36,6 +38,31 @@ async def roll(message):
 	if len(roll_strings) > 200:
 		roll_strings = "lots of dice"
 	await reply(message, "{m.author.name} rolled {}: [{}] = {}", body, roll_strings, result, m=message)
+
+
+@register
+async def test(message):
+	"""Does a standard 3d6 + bonus ability test, identifying the drama die and stunt doubles."""
+	body = ' '.join(message.content.split(' ')[1:])
+	try:
+		bonus = int(body)
+	except ValueError:
+		await reply(message, "I don't understand. Syntax: !test <bonus or penalty>, eg. !test -1")
+		return
+	# last roll is drama die
+	rolls = [random.randint(1, 6) for _ in range(3)]
+	result = sum(rolls) + bonus
+	drama = rolls[-1]
+	stunt = max(Counter(rolls).values()) > 1
+	# formatting
+	main_response = "*{r[0]}* + *{r[1]}* + __*{r[2]}*__ + {bonus} = **{result}**".format(
+		r=rolls, bonus=bonus, result=result,
+	)
+	if stunt:
+		full_response = "Stunt! {} and you can spend {} SP".format(main_response, drama)
+	else:
+		full_response = "{} with {} on the drama die".format(main_response, drama)
+	await reply(message, "{}: {}", message.author.name, full_response)
 
 
 @client.event
